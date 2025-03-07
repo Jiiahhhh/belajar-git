c89c8d544895f4dfa8512885b778952a7fe20602
ilal <ijlalnurhadi01@gmail.com>
2025-03-07 23:22:52 +0800

    add calculator.java

---
+++ Calculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kalkulator Sederhana");
+        System.out.print("Masukkan angka pertama: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Pembagian dengan nol tidak diperbolehkan.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operator tidak valid!");
+                return;
+        }
+
+        System.out.println("Hasil: " + result);
+        scanner.close();
+    }
+}
